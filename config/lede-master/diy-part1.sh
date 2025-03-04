279be270805e332450d25335a7f3f830f51d0afc
aguicc <33775575+aguicc@users.noreply.github.com>
2025-03-04 22:05:43 +0800

    Update diy-part1.sh

--- config/lede-master/diy-part1.sh
+++ config/lede-master/diy-part1.sh
@@ -13,7 +13,8 @@
 #sed -i "/helloworld/d" "feeds.conf.default"
 
 # 添加 helloworld 源
-echo "src-git packages https://github.com/coolsnowwolf/packages
+echo "
+src-git packages https://github.com/coolsnowwolf/packages
 #src-git luci https://github.com/coolsnowwolf/luci
 src-git luci https://github.com/coolsnowwolf/luci.git;openwrt-23.05
 src-git routing https://github.com/coolsnowwolf/routing
@@ -23,7 +24,8 @@ src-git helloworld https://github.com/fw876/helloworld.git
 #src-git video https://github.com/openwrt/video.git
 #src-git targets https://github.com/openwrt/targets.git
 #src-git oldpackages http://git.openwrt.org/packages.git
-src-link custom /usr/src/openwrt/custom-feed" >> "feeds.conf.default"
+src-link custom /usr/src/openwrt/custom-feed
+" >> "feeds.conf.default"
 
 # other
 # rm -rf package/lean/{samba4,luci-app-samba4,luci-app-ttyd}
